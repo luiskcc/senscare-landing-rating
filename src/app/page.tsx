3e962a502fbad2d14b7ab78e9c86a5eb8d53b0c0
Luisk <114909465+luiskcc@users.noreply.github.com>
2025-06-11 11:00:41 -0700

    added stars to title

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,19 +5,19 @@ import type { Metadata } from "next";
 export const metadata: Metadata = {
   metadataBase: new URL('https://senscarespa.com'),
   title: "SensCare Spa - Feedback",
-  description: "SensCare Spa - Feedback",
+  description: "SensCare Spa - Feedback ★★★★★",
   icons: {
     icon: "/senscare-favicon.ico",
   },
   openGraph: {
     title: "SensCare Spa - Feedback",
-    description: "SensCare Spa - Feedback",
+    description: "SensCare Spa - Feedback ★★★★★",
     images: "/og-image.png",
   },
   twitter: {
     card: "summary_large_image",
     title: "SensCare Spa - Feedback",
-    description: "SensCare Spa - Feedback",
+    description: "SensCare Spa - Feedback ★★★★★",
     images: "/og-image.png",
   },
   alternates: {
