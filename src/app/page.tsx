3f43ac4d0d52a569af38c509a9bfd30fc006aaa1
Luisk <114909465+luiskcc@users.noreply.github.com>
2025-06-10 18:30:35 -0700

    added meta data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,35 @@
 import Header from "./Header";
-import Footer from "./Footer";
 import FeedbackForm from "./FeedbackForm";
+
+export const metadata = {
+  title: "SensCare Spa - Feedback",
+  description: "SensCare Spa - Feedback",
+  icons: {
+    icon: "/senscare-favicon.ico",
+  },
+  openGraph: {
+    title: "SensCare Spa - Feedback",
+    description: "SensCare Spa - Feedback",
+    images: "/senscare-logo.svg",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "SensCare Spa - Feedback",
+    description: "SensCare Spa - Feedback",
+    images: "/senscare-logo.svg",
+  },
+  alternates: {
+    canonical: "https://senscarespa.com/feedback",
+  },
+
+  favicon: {
+    icon: "/senscare-favicon.ico",
+  },
+  manifest: "/manifest.json",
+  robots: {
+    index: true,
+  },
+}
 
 export default function Home() {
   return (
